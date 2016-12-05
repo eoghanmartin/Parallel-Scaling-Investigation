a23d35f659006cecbc51da577ac174ec4bb12d56
eoghanmartin <martine4@tcd.ie>
2016-12-05 15:55:35 -0500

    first try multi thread

--- TATAS_noshare.cpp
+++ TATAS_noshare.cpp
@@ -38,6 +38,7 @@
 //
 
 //#include "stdafx.h"                             // pre-compiled headers
+#include <mpi.h>
 #include <iostream>
 #include <iomanip>                              // setprecision
 #include "helper1.h"
@@ -571,8 +572,31 @@ int main()
             //
             waitForThreadsToFinish(nt, threadH);
             */
+            // Initialize the MPI environment
+    MPI_Init(NULL, NULL);
+
+    // Get the number of processes
+    int world_size;
+    MPI_Comm_size(MPI_COMM_WORLD, &world_size);
+
+    // Get the rank of the process
+    int world_rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &world_rank);
+
+    // Get the name of the processor
+    char processor_name[MPI_MAX_PROCESSOR_NAME];
+    int name_len;
+    MPI_Get_processor_name(processor_name, &name_len);
+
             worker();
-            double rt = (double)(clock() - start) * 1000.0 / CLOCKS_PER_SEC;;
+            double rt = (double)(clock() - start) * 1000.0 / CLOCKS_PER_SEC;
+            // Print off a hello world message
+    printf("Hello world from processor %s, rank %d"
+           " out of %d processors\n",
+           processor_name, world_rank, world_size);
+
+    // Finalize the MPI environment.
+    MPI_Finalize();
             //UINT64 rt = gettimeofday() - tstart;
 
             //
