a6a13c8776f6db539eb1a2e3d219940c65b55d16
eoghanmartin <martine4@tcd.ie>
2016-12-05 14:51:24 -0500

    mpi.sh

--- mpi_script.sh
+++ mpi_script.sh
@@ -1,11 +1,11 @@
 #!/bin/csh
 
-#$ -M netid@nd.edu	 # Email address for job notification
-#$ -m abe		 # Send mail when job begins, ends and aborts
 #$ -pe mpi-12 12	 # Specify parallel environment and legal core size
 #$ -q long		 # Specify queue
-#$ -N job_name	         # Specify job name
+#$ -N mpi_test	         # Specify job name
 
-module load xyz	         # Required modules
+module load mpich	         # Required modules
 
-mpiexec -n $NSLOTS ./app # Application to execute
+mpicc -o mpi mpi.c
+
+mpiexec -n $NSLOTS ./mpi # Application to execute
