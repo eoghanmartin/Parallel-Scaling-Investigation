0028c777b4c27b6562f109231c7502fbece9e9b8
Eoghan Martin <emarti17@ND.EDU>
2016-12-07 22:37:22 -0500

    ** with no

--- TATAS_noshare.cpp
+++ TATAS_noshare.cpp
@@ -238,29 +238,29 @@ void BST::add (Node *n)
     //acquireTATAS();
     Node* volatile* volatile pp = &root;
     Node* volatile p = root;
-    Node* lockedNode = new Node();
+    Node lockedNode = new Node();
     while (p) {
         if (n->key < p->key) {
             if (&p->left == NULL){
-                lockedNode = pp;
-                *lockedNode.acquireTATAS_node();
+                lockedNode = **pp;
+                lockedNode.acquireTATAS_node();
             }
             pp = &p->left;
         } else if (n->key > p->key) {
             if (&p->right == NULL){
-                lockedNode = pp;
-                *lockedNode.acquireTATAS_node();
+                lockedNode = **pp;
+                lockedNode.acquireTATAS_node();
             }
             pp = &p->right;
         } else {
-            *lockedNode.releaseTATAS_node();
+            lockedNode.releaseTATAS_node();
             //releaseTATAS();
             return;
         }
         p = *pp;
     }
     *pp = n;
-    *lockedNode.releaseTATAS_node();
+    lockedNode.releaseTATAS_node();
     //releaseTATAS();
 }
 
