7e0cab638d13119b61416cd5d94ab761804e3b2f
Eoghan Martin <emarti17@lib-1911.library.nd.edu>
2016-12-13 16:53:12 -0500

    num threads

--- TATAS_nodeLocks_Threaded.cpp
+++ TATAS_nodeLocks_Threaded.cpp
@@ -406,21 +406,21 @@ int main()
 
         UINT64 n = 0;
 
-        while(1){
-            if (((double)(clock() - start) * 1000.0) / CLOCKS_PER_SEC > NSECONDS*1000) {
-                        break;
-                    }
+        //while(1){
+            //if (((double)(clock() - start) * 1000.0) / CLOCKS_PER_SEC > NSECONDS*1000) {
+            //            break;
+            //        }
 
             #pragma omp parallel default(shared) private(iam, np)
             {
-                np = omp_get_num_threads();
+                np = 2;//omp_get_num_threads();
                 iam = omp_get_thread_num();
                 //printf("Hello from thread %d out of %d from process %d out of %d on %s\n", iam, np, rank, numprocs, processor_name);
 
-                //while(1){
-                    //if (((double)(clock() - start) * 1000.0) / CLOCKS_PER_SEC > NSECONDS*1000) {
-                    //    break;
-                    //}
+                while(1){
+                    if (((double)(clock() - start) * 1000.0) / CLOCKS_PER_SEC > NSECONDS*1000) {
+                        break;
+                    }
                     MPI_Recv(&message_recv, 2, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status_master);
                     randomBit_recv = message_recv[1];
                     runOp(randomValue_recv, randomBit_recv);
@@ -428,9 +428,9 @@ int main()
                     MPI_Send(&world_rank, 1, MPI_INT, status_master.MPI_SOURCE, 1, MPI_COMM_WORLD);
                     n += 1;
                     cout << n << endl;
-                //}
+                }
             }
-        }
+        //}
         BinarySearchTree->destroy(BinarySearchTree->root); //Recursively destroy BST
         BinarySearchTree->root = NULL;
 
