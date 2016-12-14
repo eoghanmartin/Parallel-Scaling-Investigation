50fe2b982cbf9bb5cdc96be4afcd97d9ddbd9a60
Eoghan Martin <emarti17@lib-1911.library.nd.edu>
2016-12-13 19:34:08 -0500

    4 threads

--- TATAS_singleLock_OMP.cpp
+++ TATAS_singleLock_OMP.cpp
@@ -375,7 +375,7 @@ int main()
     UINT randomBit;
 
     omp_set_dynamic(0);     // Explicitly disable dynamic teams
-    omp_set_num_threads(4);
+    omp_set_num_threads(16);
     #pragma omp parallel private(iam, np, thread_count, randomBit)
     {
         np = numThreads = omp_get_num_threads();
