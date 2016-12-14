285608322a9e5d622b21c53cb72312a9694e667a
Eoghan Martin <emarti17@ND.EDU>
2016-12-14 15:43:12 -0500

    &pp->lock_node

--- TATAS_nodeLocks_OMP.cpp
+++ TATAS_nodeLocks_OMP.cpp
@@ -341,7 +341,7 @@ void Node::acquireTATAS_node() {
 
 void BST::acquireTATAS_node(Node* pp) {
     volatile long int *p = &pp->lock_node;
-    while (InterlockedExchange(**p, 1) == 1){
+    while (InterlockedExchange(&pp->lock_node, 1) == 1){
         do {
             cout << "acquiring" << endl;
             _mm_pause();
